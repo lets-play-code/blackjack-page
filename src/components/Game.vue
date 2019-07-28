594bbfcc54f3423ef8d171ae7fb1fcb38f11e166
Vic Wu <madcocomo@gmail.com>
2019-07-28 15:35:55 +0800

    closeDeal

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -2,6 +2,8 @@
     <div>
         <h1> Start Play Black Jack </h1>
         <button v-on:click='getCards'>Start</button>
+        <button v-on:click='closeDeal'>CloseDeal</button>
+        <h1 v-if="this.result">  WINNER : {{ this.result.host.winner ? 'HOST' : 'PLAYER'}}</h1>
         <p>Host: </p>
         <p><Card v-for="card in game.host" :key='card' :code='card'/></p>
         <p>Player: </p>
@@ -11,6 +13,7 @@
 
 <script>
 const START_GAME = 'startgame'
+const CLOSE_DEAL = 'closedeal'
 import Card from './Card.vue'
 export default {
     name: 'Game',
@@ -19,13 +22,21 @@ export default {
     },
     data () {
         return {
-            game: ''
+            game: '',
+            result: null
         }
     },
     methods: {
         getCards () {
             this.$http.post(START_GAME).then(response => {
                 this.game = response.data
+            }, error => {
+                error
+            });
+        },
+        closeDeal () {
+            this.$http.post(CLOSE_DEAL).then(response => {
+                this.result = response.data
             }, error => {
                 error
             });
