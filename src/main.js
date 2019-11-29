685ecf895ee69f0de33903baebdb3d168350079a
Vic Wu <madcocomo@gmail.com>
2019-11-29 15:25:59 +0800

    add devserver proxy

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import Resource from 'vue-resource'
 
 Vue.use(Resource)
 Vue.config.productionTip = false
-Vue.http.options.root = 'http://localhost:8080'
 new Vue({
   render: h => h(App),
 }).$mount('#app')
